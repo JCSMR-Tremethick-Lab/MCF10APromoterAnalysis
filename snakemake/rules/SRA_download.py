31ae737886adfba4a646eada2d7bd801a7241507
Sebastian Kurscheid <genomicsio@gmail.com>
2016-06-09 15:28:02 +1000

    implements SRA download and fastq-dump; closes issue #1

--- snakemake/rules/SRA_download.py
+++ snakemake/rules/SRA_download.py
@@ -17,24 +17,35 @@ import snakemake.utils
 
 SRA_IDS, = glob_wildcards("SRA/requested/{id}")
 
-
 def make_sra_urls(wildcards):
-    url_prefix = "ftp://ftp-trace.ncbi.nih.gov/sra/sra-instant/reads/ByRun/sra"
     url = []
-    for i in wildcards["sra_ids"]:
-        url.append(url_prefix + "/" + i[0:3] + "/" + i[0:6] + "/" + i + "/" + i + ".sra")
+    url_prefix = "ftp://ftp-trace.ncbi.nih.gov/sra/sra-instant/reads/ByRun/sra"
+    i = wildcards['sra_ids']
+    url.append(url_prefix + "/" + i[0:3] + "/" + i[0:6] + "/" + i + "/" + i + ".sra")
     return(url)
 
 rule download_sra:
-    input:
-        "SRA/requested/{sra_ids}"
+    params:
+        url = make_sra_urls
     output:
         "SRA/sra/{sra_ids}.sra"
     shell:
         """
-            prefetch {wildcards.sra_ids} --ascp-options --move-after-transfer=./SRA/sra/
+            wget {params.url} {output}
+        """
+
+rule fastq_dump:
+    input:
+        rules.download_sra.output
+    output:
+        "SRA/fastq/{sra_ids}_1.fastq.gz",
+        "SRA/fastq/{sra_ids}_2.fastq.gz"
+    shell:
+        """
+            fastq-dump --split-files --gzip {input}
         """
 
 rule download:
     input:
-        expand("SRA/sra/{sra_ids}.sra", sra_ids = SRA_IDS)
+        expand("SRA/fastq/{sra_ids}_1.fastq.gz", sra_ids = SRA_IDS),
+        expand("SRA/fastq/{sra_ids}_2.fastq.gz", sra_ids = SRA_IDS)
