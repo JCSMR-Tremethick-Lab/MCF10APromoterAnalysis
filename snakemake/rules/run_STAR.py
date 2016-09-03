fb6f728258101b0e8def0e088eed9a1bd08e22cd
Sebastian Kurscheid <genomicsio@gmail.com>
2016-09-03 12:40:30 +1000

    bug fix

--- snakemake/rules/run_STAR.py
+++ snakemake/rules/run_STAR.py
@@ -13,8 +13,8 @@ rule star_align_full:
         runThreadN = config["STAR"]["runThreadN"],
         trim_dir = config["trim_dir"]
     input:
-        read1 = "./{assayID}/{runID}/{processed_dir}/{params.trim_dir}/{unit}_R1_001.QT.CA.fastq.gz",
-        read2 = "./{assayID}/{runID}/{processed_dir}/{params.trim_dir}/{unit}_R2_001.QT.CA.fastq.gz",
+        read1 = "./{assayID}/{runID}/{processed_dir}/{trim_dir}/{unit}_R1_001.QT.CA.fastq.gz",
+        read2 = "./{assayID}/{runID}/{processed_dir}/{trim_dir}/{unit}_R2_001.QT.CA.fastq.gz",
         index = lambda wildcards: config["references"]["hg19"]["STAR"][wildcards.reference_version]
     output:
         "./{assayID}/{runID}/{processed_dir}/{reference_version}/STAR/full/{unit}.aligned.bam"
