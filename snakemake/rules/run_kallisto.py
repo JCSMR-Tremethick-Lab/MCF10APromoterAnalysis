abe93300c08508680f265dd4cad8f36adac6b01c
Sebastian Kurscheid <genomicsio@gmail.com>
2016-09-03 12:47:59 +1000

    bug fix

--- snakemake/rules/run_kallisto.py
+++ snakemake/rules/run_kallisto.py
@@ -14,8 +14,8 @@ rule kallisto_quant:
         threads = 4,
         trim_dir = config["trim_dir"]
     input:
-        read1 = "./{assayID}/{runID}/{processed_dir}/{trim_data}/{unit}_R1_001.QT.CA.fastq.gz",
-        read2 = "./{assayID}/{runID}/{processed_dir}/{trim_data}/{unit}_R2_001.QT.CA.fastq.gz",
+        read1 = "./{assayID}/{runID}/{processed_dir}/{params.trim_dir}/{unit}_R1_001.QT.CA.fastq.gz",
+        read2 = "./{assayID}/{runID}/{processed_dir}/{params.trim_dir}/{unit}_R2_001.QT.CA.fastq.gz",
         ki = lambda wildcards: config["references"]["hg19"]["kallisto"][wildcards.reference_version]
     output:
         protected("./{assayID}/{runID}/{processed_dir}/{reference_version}/kallisto/{unit}")
