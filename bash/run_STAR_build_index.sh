12c92336ea4b1f73240dae926e29e461c9cbae8a
Sebastian Kurscheid <genomicsio@gmail.com>
2016-09-05 14:25:43 +1000

    adds command to build hg19 based STAR index

--- bash/run_STAR_build_index.sh
+++ bash/run_STAR_build_index.sh
@@ -4,3 +4,11 @@ STAR --runThreadN 64 \
      --genomeFastaFiles /home/skurscheid/Data/References/Genomes/Homo_sapiens/GRCh38_ensembl84/primary/Homo_sapiens.GRCh38.dna.primary_assembly.fa ~/Data/References/Transcriptomes/ERCC/ERCC92.fa \
      --sjdbGTFfile /home/skurscheid/Data/References/Annotations/Homo_sapiens/GRCh38_ensembl84/Homo_sapiens.GRCh38.84_ERCC.gtf \
      --sjdbOverhang 76
+
+
+STAR --runThreadN 96 \
+     --runMode genomeGenerate \
+     --genomeDir ./STAR_Index_ERCC/ \
+     --genomeFastaFiles /home/skurscheid/Data/References/Genomes/Homo_sapiens/GRCh37_hg19_ensembl75/primary/Homo_sapiens.GRCh37.75.dna.primary_assembly.fa ~/Data/References/Transcriptomes/ERCC/ERCC92.fa \
+     --sjdbGTFfile /home/skurscheid/Data/References/Annotations/Homo_sapiens/GRCh37_hg19_ensembl75/Homo_sapiens.GRCh37.75_ERCC.gtf \
+     --sjdbOverhang 76
