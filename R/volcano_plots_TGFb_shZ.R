48cbcdedd628dc75a24a055254941a19beea9887
Sebastian Kurscheid <genomicsio@gmail.com>
2016-11-25 14:10:53 +1100

    makes modifications to accommodate analysis of second RNA-Seq run data

--- R/volcano_plots_TGFb_shZ.R
+++ R/volcano_plots_TGFb_shZ.R
@@ -1,11 +1,11 @@
 # volcano plot - TGFb vs WT
-emtGenes <- resultsCompressed[["MCF10A"]]$sleuth_results.gene[["conditionMCF10A_TGFb"]][which(resultsCompressed[["MCF10A"]]$sleuth_results.gene[["conditionMCF10A_TGFb"]]$target_id %in% hsap.qPCRGenesTab$ensembl_gene_id),]
+emtGenes <- resultsCompressed[[2]]$sleuth_results_genes[["conditionMCF10A_TGFb"]][which(resultsCompressed[[2]]$sleuth_results_genes[["conditionMCF10A_TGFb"]]$target_id %in% hsap.qPCRGenesTab$ensembl_gene_id),]
 emtGenes$FC_estimated <- log2(exp(emtGenes$b))
 emtGenes <- merge(emtGenes, ensGenes[,c("ensembl_gene_id", "external_gene_name")], by.x = "target_id", by.y = "ensembl_gene_id")
 pdf("Volcano_plot_EMT_genes_TGFb.pdf")
 plot(emtGenes$b, -log10(emtGenes$qval), axes = F, xlab = "", ylab = "", frame = F, xlim = c(-10,10), cex = 0.3, pch = 16)
 points(emtGenes[which(-log10(emtGenes$qval) >= 1), "b"], -log10(emtGenes[which(-log10(emtGenes$qval) >= 1), "qval"]), col = "red", pch = 16, cex = 1.2)
-axis(2, pos = 0, lwd = 3, at = c(seq(0,20,2)), labels = c("", seq(2,20,2)))
+axis(2, pos = 0, lwd = 3) #, at = c(seq(0,200,2)), labels = c("", seq(2,200,2)))
 axis(1, pos = 0, lwd = 3)
 mtext("-log10(q-value)", side = 2)
 mtext("beta value", side = 1, line = 2)
@@ -19,13 +19,13 @@ text(-4,1.2, "< 0.1 [adjusted p-value]", cex = 0.7)
 dev.off()
 
 # volcano plot - shZ vs WT
-emtGenes.shZ <- resultsCompressed[["MCF10A"]]$sleuth_results.gene[["conditionMCF10A_shZ"]][which(resultsCompressed[["MCF10A"]]$sleuth_results.gene[["conditionMCF10A_shZ"]]$target_id %in% hsap.qPCRGenesTab$ensembl_gene_id),]
+emtGenes.shZ <- resultsCompressed[[1]]$sleuth_results_genes[["conditionMCF10A_shZ"]][which(resultsCompressed[[1]]$sleuth_results_genes[["conditionMCF10A_shZ"]]$target_id %in% hsap.qPCRGenesTab$ensembl_gene_id),]
 emtGenes.shZ$FC_estimated <- log2(exp(emtGenes.shZ$b))
 emtGenes.shZ <- merge(emtGenes.shZ, ensGenes[,c("ensembl_gene_id", "external_gene_name")], by.x = "target_id", by.y = "ensembl_gene_id")
 pdf("Volcano_plot_EMT_genes_shZ.pdf")
 plot(emtGenes.shZ$b, -log10(emtGenes.shZ$qval), axes = F, xlab = "", ylab = "", frame = F, xlim = c(-10,10), cex = 0.3, pch = 16)
 points(emtGenes.shZ[which(-log10(emtGenes.shZ$qval) >= 1), "b"], -log10(emtGenes.shZ[which(-log10(emtGenes.shZ$qval) >= 1), "qval"]), col = "red", pch = 16, cex = 1.2)
-axis(2, pos = 0, lwd = 3, at = c(seq(0,20,2)), labels = c("", seq(2,20,2)))
+axis(2, pos = 0, lwd = 3) #, at = c(seq(0,20,2)), labels = c("", seq(2,20,2)))
 axis(1, pos = 0, lwd = 3)
 mtext("-log10(q-value)", side = 2)
 mtext("beta value", side = 1, line = 2)
