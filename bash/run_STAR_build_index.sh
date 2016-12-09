175e9549ff4308bb3363a0ef8daeb0a38862fcd7
Sebastian Kurscheid <genomicsio@gmail.com>
2016-12-09 16:38:46 +1100

    adds new CLI command

--- bash/run_STAR_build_index.sh
+++ bash/run_STAR_build_index.sh
@@ -1,14 +1,21 @@
 STAR --runThreadN 64 \
      --runMode genomeGenerate \
      --genomeDir ./STAR_Index_ERCC/ \
-     --genomeFastaFiles /home/skurscheid/Data/References/Genomes/Homo_sapiens/GRCh38_ensembl84/primary/Homo_sapiens.GRCh38.dna.primary_assembly.fa ~/Data/References/Transcriptomes/ERCC/ERCC92.fa \
-     --sjdbGTFfile /home/skurscheid/Data/References/Annotations/Homo_sapiens/GRCh38_ensembl84/Homo_sapiens.GRCh38.84_ERCC.gtf \
+     --genomeFastaFiles /home/sebastian/Data/References/Genomes/Homo_sapiens/GRCh38_ensembl84/primary/Homo_sapiens.GRCh38.dna.primary_assembly.fa ~/Data/References/Transcriptomes/ERCC/ERCC92.fa \
+     --sjdbGTFfile /home/sebastian/Data/References/Annotations/Homo_sapiens/GRCh38_ensembl84/Homo_sapiens.GRCh38.84_ERCC.gtf \
      --sjdbOverhang 76
 
 
 STAR --runThreadN 96 \
      --runMode genomeGenerate \
      --genomeDir ./STAR_Index_ERCC/ \
-     --genomeFastaFiles /home/skurscheid/Data/References/Genomes/Homo_sapiens/GRCh37_hg19_ensembl75/primary/Homo_sapiens.GRCh37.75.dna.primary_assembly.fa ~/Data/References/Transcriptomes/ERCC/ERCC92.fa \
-     --sjdbGTFfile /home/skurscheid/Data/References/Annotations/Homo_sapiens/GRCh37_hg19_ensembl75/Homo_sapiens.GRCh37.75_ERCC.gtf \
+     --genomeFastaFiles /home/sebastian/Data/References/Genomes/Homo_sapiens/GRCh37_hg19_ensembl75/primary/Homo_sapiens.GRCh37.75.dna.primary_assembly.fa ~/Data/References/Transcriptomes/ERCC/ERCC92.fa \
+     --sjdbGTFfile /home/sebastian/Data/References/Annotations/Homo_sapiens/GRCh37_hg19_ensembl75/Homo_sapiens.GRCh37.75_ERCC.gtf \
+     --sjdbOverhang 76
+
+STAR --runThreadN 32 \
+     --runMode genomeGenerate \
+     --genomeDir ./STAR_Index/ \
+     --genomeFastaFiles /home/sebastian/Data/References/Genomes/Homo_sapiens/GRCh37_hg19_ensembl75/primary/Homo_sapiens.GRCh37.75.dna.primary_assembly.fa \
+     --sjdbGTFfile /home/sebastian/Data/References/Annotations/Homo_sapiens/GRCh37_hg19_ensembl75/Homo_sapiens.GRCh37.75.gtf \
      --sjdbOverhang 76
