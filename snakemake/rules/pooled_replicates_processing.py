f2b9da6faf4d0f2bb532f095e1a3302835d056f5
Sebastian Kurscheid <genomicsio@gmail.com>
2017-02-08 15:22:53 +1100

    adds rule to run computeMatrix on pooled replicates coverage data

---
+++ snakemake/rules/pooled_replicates_processing.py
@@ -0,0 +1,59 @@
+from snakemake.exceptions import MissingInputException
+import os
+
+home = os.environ['HOME']
+
+REF_GENOME = config["references"]["genomes"][1]
+
+# run parameters as variables
+ASSAYID = "ChIP-Seq"
+RUNID = "merged"
+OUTDIR = config["processed_dir"]
+REFVERSION = reference_version = config["references"][REF_GENOME]["version"][0]
+QUALITY = config["alignment_quality"]
+
+def cli_parameters_computeMatrix(wildcards):
+    a = config["program_parameters"][wildcards["application"]]["computeMatrix"][wildcards["command"]]
+    if wildcards["command"] == "reference-point":
+        a["--referencePoint"] = wildcards.referencePoint
+    return(a)
+
+rule run_computeMatrix_pooled_replicates:
+    input:
+        expand("{assayID}/{runID}/{outdir}/{reference_version}/{application}/computeMatrix/{command}/{duplicates}/{referencePoint}/{sampleGroup}_{region}_{mode}.matrix.gz",
+               assayID = ASSAYID,
+               runID = RUNID,
+               outdir = OUTDIR,
+               reference_version = REFVERSION,
+               application = "deepTools",
+               command = ["reference-point", "scale-regions"],
+               duplicates = ["duplicates_marked", "duplicates_removed"],
+               referencePoint = "TSS",
+               sampleGroup = ["H2AZ_10A_high", "H2AZ_TGFb_10A", "Inp_10A_WT_high", "Inp_10A_TGFb_high", "Inp_shZ_10A_high"],
+               region = ["allGenes", "TanEMTup", "TanEMTdown"],
+               mode = ["MNase", "normal"])
+
+rule computeMatrix_pooled_replicates:
+    version:
+        0.2
+    params:
+        deepTools_dir = home + config["deepTools_dir"],
+        program_parameters = lambda wildcards: ' '.join("{!s}={!s}".format(key, val.strip("\\'")) for (key, val) in cli_parameters_computeMatrix(wildcards).items())
+    threads:
+        lambda wildcards: int(str(config["program_parameters"]["deepTools"]["threads"]).strip("['']"))
+    input:
+        file = "{assayID}/{runID}/{outdir}/{reference_version}/{application}/bamCoverage/{mode}/{duplicates}/{sampleGroup}_{mode}_RPKM.bw",
+        region = lambda wildcards: home + config["program_parameters"]["deepTools"]["regionFiles"][wildcards.region]
+    output:
+        matrix_gz = "{assayID}/{runID}/{outdir}/{reference_version}/{application}/computeMatrix/{command}/{duplicates}/{referencePoint}/{sampleGroup}_{region}_{mode}.matrix.gz"
+    shell:
+        """
+            {params.deepTools_dir}/computeMatrix {wildcards.command} \
+                                                 --regionsFileName {input.region} \
+                                                 --scoreFileName {input.file} \
+                                                 --missingDataAsZero \
+                                                 --skipZeros \
+                                                 --numberOfProcessors {threads} \
+                                                 {params.program_parameters} \
+                                                 --outFileName {output.matrix_gz}
+        """
