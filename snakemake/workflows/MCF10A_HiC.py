84dfca54fe6dbb000fb1478fe95c1c4f436cda1b
Sebastian Kurscheid <genomicsio@gmail.com>
2017-08-02 14:28:53 +0200

    adds run command for armatus

--- snakemake/workflows/MCF10A_HiC.py
+++ snakemake/workflows/MCF10A_HiC.py
@@ -16,17 +16,34 @@ wrapper_dir = os.environ['HOME'] + "/Development/snakemake-wrappers/bio"
 
 include_prefix= os.environ['HOME'] + "/Development/JCSMR-Tremethick-Lab/Breast/snakemake/rules/"
 
+armatus_bin = os.environ['HOME'] + "/bin/armatus-linux-x64"
+
 rule prepare_armatus_input:
     version: 0.1
     input:
         "{sample}.{chr1}.{chr2}.{res}.mat"
     output:
-        "{sample}.{chr1}.{chr2}.{res}.tsv"
+        "{sample}.{chr1}.{chr2}.{res}.gz"
     threads: 1
     shell:
         """
-            cut -f 2- -d " " {input} | sed "1d" - | tr " " "\t" > {output}
+            cut -f 2- -d " " {input} | sed "1d" - | tr " " "\t" | gzip > {output}
         """
+
+rule run_armatus:
+    version:
+        0.1
+    input:
+        "{sample}.{chr1}.{chr2}.{res}.gz"
+    output:
+        "{sample}.{chr1}.{chr2}.{res}.consensus.txt"
+    params:
+        prefix = "{sample}.{chr1}.{chr2}.{res}"
+    shell:
+        """
+            armatus_bin -i {input} -g 1.0 -s 0.5 -o {params.prefix} -r 40000 -c {wildcards.chr1}
+        """
+
 
 rule all:
     input:
