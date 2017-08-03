6b214a56a951ff5ef0749fdbbb9d4dd475fdfba0
Sebastian Kurscheid <genomicsio@gmail.com>
2017-08-03 14:32:33 +0200

    fixes problem with chromosome names

--- bash/convert_list_to_coo.sh
+++ bash/convert_list_to_coo.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-sed 's/-/ /g' $1 | sed 's/k/000/g' | awk '{if($5!=0.0) printf("%s\t%s\t%s\n",$2-40000,$4-40000,$5)}' > $2
+sed 's/-/ /g' $1 | sed 's/k/000/g' | awk '{if($5!=0.0) printf("chr%s\t%s\t%s\n",$2-40000,$4-40000,$5)}' > $2
