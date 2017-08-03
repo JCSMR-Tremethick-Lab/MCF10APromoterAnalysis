d73ec4a1b2f5e81aca1da6cc0d4714aa68f4069c
Sebastian Kurscheid <genomicsio@gmail.com>
2017-08-03 15:25:08 +0200

    adds scripts to generate visualisation tracks

---
+++ bash/make_gtrack.sh
@@ -0,0 +1,3 @@
+#! /bin/sh
+python ~/Development/JCSMR-Tremethick-Lab/Breast/Python/make_gtrack.py $1 $2 | sort -k1,1 -k2,2n > $3
+sed -i  '1s/^/##gtrack version: 1.0\n##track type: linked segments\n###seqid\tstart\tend\tid\tradius\tedges\n/' $3
