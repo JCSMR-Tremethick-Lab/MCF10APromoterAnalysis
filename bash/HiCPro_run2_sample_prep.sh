5d7d99a39f726440ac2a044225beecc7c8cb6ee2
Sebastian Kurscheid <genomicsio@gmail.com>
2018-02-12 13:42:34 +1100

    adds scripts and configs for processing HiC data

---
+++ bash/HiCPro_run2_sample_prep.sh
@@ -0,0 +1,77 @@
+HiC_7# MCF10A_WT
+mkdir MCF10A_WT_1 ;cd MCF10A_WT_1
+ln -s /home/skurscheid/Data/Tremethick/Breast/HiC/raw_data/170306_NB501086_0102_HiC1_6_run4/HiC_MCF_WT_1_r4/HiC_2_r4_S2_R1_001.fastq.gz
+ln -s /home/skurscheid/Data/Tremethick/Breast/HiC/raw_data/170306_NB501086_0102_HiC1_6_run4/HiC_MCF_WT_1_r4/HiC_2_r4_S2_R2_001.fastq.gz
+ln -s /home/skurscheid/Data/Tremethick/Breast/HiC/raw_data/NB501086_0100_DTremethick_HiC1_6_run3/HiC_MCF_WT_1_r3/HiC_2_r3_S2_R1_001.fastq.gz
+ln -s /home/skurscheid/Data/Tremethick/Breast/HiC/raw_data/NB501086_0100_DTremethick_HiC1_6_run3/HiC_MCF_WT_1_r3/HiC_2_r3_S2_R2_001.fastq.gz
+cd ..
+
+mkdir MCF10A_WT_2 ;cd MCF10A_WT_2
+ln -s /home/skurscheid/Data/Tremethick/Breast/HiC/raw_data/170306_NB501086_0102_HiC1_6_run4/HiC_MCF_WT_2_r4/HiC_6_r4_S6_R1_001.fastq.gz
+ln -s /home/skurscheid/Data/Tremethick/Breast/HiC/raw_data/170306_NB501086_0102_HiC1_6_run4/HiC_MCF_WT_2_r4/HiC_6_r4_S6_R2_001.fastq.gz
+ln -s /home/skurscheid/Data/Tremethick/Breast/HiC/raw_data/NB501086_0100_DTremethick_HiC1_6_run3/HiC_MCF_WT_2_r3/HiC_6_r3_S6_R1_001.fastq.gz
+ln -s /home/skurscheid/Data/Tremethick/Breast/HiC/raw_data/NB501086_0100_DTremethick_HiC1_6_run3/HiC_MCF_WT_2_r3/HiC_6_r3_S6_R2_001.fastq.gz
+cd ..
+
+mkdir MCF10AT1_1; cd MCF10AT1_1
+ln -s /home/skurscheid/Data/Tremethick/Breast/HiC/raw_data/170306_NB501086_0102_HiC1_6_run4/HiC_MCF_T1_1_r4/HiC_3_r4_S3_R1_001.fastq.gz
+ln -s /home/skurscheid/Data/Tremethick/Breast/HiC/raw_data/170306_NB501086_0102_HiC1_6_run4/HiC_MCF_T1_1_r4/HiC_3_r4_S3_R2_001.fastq.gz
+ln -s /home/skurscheid/Data/Tremethick/Breast/HiC/raw_data/NB501086_0100_DTremethick_HiC1_6_run3/HiC_MCF_T1_1_r3/HiC_3_r3_S3_R1_001.fastq.gz
+ln -s /home/skurscheid/Data/Tremethick/Breast/HiC/raw_data/NB501086_0100_DTremethick_HiC1_6_run3/HiC_MCF_T1_1_r3/HiC_3_r3_S3_R2_001.fastq.gz
+cd ..
+
+mkdir MCF10AT1_2; cd MCF10AT1_2
+ln -s /home/skurscheid/Data/Tremethick/Breast/HiC/raw_data/170306_NB501086_0102_HiC1_6_run4/HiC_MCF_T1_2_r4/HiC_4_r4_S4_R1_001.fastq.gz
+ln -s /home/skurscheid/Data/Tremethick/Breast/HiC/raw_data/170306_NB501086_0102_HiC1_6_run4/HiC_MCF_T1_2_r4/HiC_4_r4_S4_R2_001.fastq.gz
+ln -s /home/skurscheid/Data/Tremethick/Breast/HiC/raw_data/NB501086_0100_DTremethick_HiC1_6_run3/HiC_MCF_T1_2_r3/HiC_4_r3_S4_R1_001.fastq.gz
+ln -s /home/skurscheid/Data/Tremethick/Breast/HiC/raw_data/NB501086_0100_DTremethick_HiC1_6_run3/HiC_MCF_T1_2_r3/HiC_4_r3_S4_R2_001.fastq.gz
+cd ..
+
+mkdir MCF10CA1A_1; cd MCF10CA1A_1
+ln -s /home/skurscheid/Data/Tremethick/Breast/HiC/raw_data/170306_NB501086_0102_HiC1_6_run4/HiC_MCF_Cancer_1_r4/HiC_1_r4_S1_R1_001.fastq.gz
+ln -s /home/skurscheid/Data/Tremethick/Breast/HiC/raw_data/170306_NB501086_0102_HiC1_6_run4/HiC_MCF_Cancer_1_r4/HiC_1_r4_S1_R2_001.fastq.gz
+ln -s /home/skurscheid/Data/Tremethick/Breast/HiC/raw_data/NB501086_0100_DTremethick_HiC1_6_run3/HiC_MCF_Cancer_1_r3/HiC_1_r3_S1_R1_001.fastq.gz
+ln -s /home/skurscheid/Data/Tremethick/Breast/HiC/raw_data/NB501086_0100_DTremethick_HiC1_6_run3/HiC_MCF_Cancer_1_r3/HiC_1_r3_S1_R2_001.fastq.gz
+cd ..
+
+mkdir MCF10CA1A_2; cd MCF10CA1A_2
+ln -s /home/skurscheid/Data/Tremethick/Breast/HiC/raw_data/170306_NB501086_0102_HiC1_6_run4/HiC_MCF_Cancer_2_r4/HiC_5_r4_S5_R1_001.fastq.gz
+ln -s /home/skurscheid/Data/Tremethick/Breast/HiC/raw_data/170306_NB501086_0102_HiC1_6_run4/HiC_MCF_Cancer_2_r4/HiC_5_r4_S5_R2_001.fastq.gz
+ln -s /home/skurscheid/Data/Tremethick/Breast/HiC/raw_data/NB501086_0100_DTremethick_HiC1_6_run3/HiC_MCF_Cancer_2_r3/HiC_5_r3_S5_R1_001.fastq.gz
+ln -s /home/skurscheid/Data/Tremethick/Breast/HiC/raw_data/NB501086_0100_DTremethick_HiC1_6_run3/HiC_MCF_Cancer_2_r3/HiC_5_r3_S5_R2_001.fastq.gz
+cd ..
+
+mkdir MCF10A_shH2AZ_1; cd MCF10A_shH2AZ_1
+ln -s /home/skurscheid/Data/Tremethick/Breast/HiC/raw_data/NB501086_0088_DTremethick_JCSMR_HiC_shZ_TGFb/HiC_2/shZ_rep1_S1_R1_001.fastq.gz shZ_rep1_r2_S1_R1_001.fastq.gz
+ln -s /home/skurscheid/Data/Tremethick/Breast/HiC/raw_data/NB501086_0088_DTremethick_JCSMR_HiC_shZ_TGFb/HiC_2/shZ_rep1_S1_R2_001.fastq.gz shZ_rep1_r2_S1_R2_001.fastq.gz
+ln -s /home/skurscheid/Data/Tremethick/Breast/HiC/raw_data/NB501086_0064_DTremethick_JCSMR_HiC_shZ_TGFb/HiC_2/shZ-rep1_S1_R1_001.fastq.gz shZ_rep1_r1_S1_R1_001.fastq.gz
+ln -s /home/skurscheid/Data/Tremethick/Breast/HiC/raw_data/NB501086_0064_DTremethick_JCSMR_HiC_shZ_TGFb/HiC_2/shZ-rep1_S1_R1_001.fastq.gz shZ_rep1_r1_S1_R2_001.fastq.gz
+ln -s /home/skurscheid/Data/Tremethick/Breast/HiC/raw_data/NB501086_0103_DTremethick_JCSMR_HiC_shZ_TGFb_run3/HiC_2_r3/shZ_rep1_r3_S1_R1_001.fastq.gz
+ln -s /home/skurscheid/Data/Tremethick/Breast/HiC/raw_data/NB501086_0103_DTremethick_JCSMR_HiC_shZ_TGFb_run3/HiC_2_r3/shZ_rep1_r3_S1_R2_001.fastq.gz
+cd ..
+
+mkdir MCF10A_shH2AZ_2; cd MCF10A_shH2AZ_2
+ln -s /home/skurscheid/Data/Tremethick/Breast/HiC/raw_data/NB501086_0088_DTremethick_JCSMR_HiC_shZ_TGFb/HiC_5/shZ_rep2_S2_R1_001.fastq.gz shZ_rep2_r2_S2_R1_001.fastq.gz
+ln -s /home/skurscheid/Data/Tremethick/Breast/HiC/raw_data/NB501086_0088_DTremethick_JCSMR_HiC_shZ_TGFb/HiC_5/shZ_rep2_S2_R2_001.fastq.gz shZ_rep2_r2_S2_R2_001.fastq.gz
+ln -s /home/skurscheid/Data/Tremethick/Breast/HiC/raw_data/NB501086_0064_DTremethick_JCSMR_HiC_shZ_TGFb/HiC_5/shZ-rep2_S2_R1_001.fastq.gz shZ_rep2_r1_S2_R1_001.fastq.gz
+ln -s /home/skurscheid/Data/Tremethick/Breast/HiC/raw_data/NB501086_0064_DTremethick_JCSMR_HiC_shZ_TGFb/HiC_5/shZ-rep2_S2_R1_001.fastq.gz shZ_rep2_r1_S2_R2_001.fastq.gz
+ln -s /home/skurscheid/Data/Tremethick/Breast/HiC/raw_data/NB501086_0103_DTremethick_JCSMR_HiC_shZ_TGFb_run3/HiC_5_r3/shZ_rep2_r3_S2_R1_001.fastq.gz
+ln -s /home/skurscheid/Data/Tremethick/Breast/HiC/raw_data/NB501086_0103_DTremethick_JCSMR_HiC_shZ_TGFb_run3/HiC_5_r3/shZ_rep2_r3_S2_R2_001.fastq.gz
+cd ..
+
+mkdir MCF10A_TGFb_1; cd MCF10A_TGFb_1
+ln -s /home/skurscheid/Data/Tremethick/Breast/HiC/raw_data/NB501086_0088_DTremethick_JCSMR_HiC_shZ_TGFb/HiC_6/TGFb_rep1_S3_R1_001.fastq.gz TGFb_rep1_r1_S3_R1_001.fastq.gz
+ln -s /home/skurscheid/Data/Tremethick/Breast/HiC/raw_data/NB501086_0088_DTremethick_JCSMR_HiC_shZ_TGFb/HiC_6/TGFb_rep1_S3_R2_001.fastq.gz TGFb_rep1_r1_S3_R2_001.fastq.gz
+ln -s /home/skurscheid/Data/Tremethick/Breast/HiC/raw_data/NB501086_0064_DTremethick_JCSMR_HiC_shZ_TGFb/HiC_6/TGFb-rep1_S3_R1_001.fastq.gz TGFb_rep1_r2_S3_R1_001.fastq.gz
+ln -s /home/skurscheid/Data/Tremethick/Breast/HiC/raw_data/NB501086_0064_DTremethick_JCSMR_HiC_shZ_TGFb/HiC_6/TGFb-rep1_S3_R2_001.fastq.gz TGFb_rep1_r2_S3_R2_001.fastq.gz
+ln -s /home/skurscheid/Data/Tremethick/Breast/HiC/raw_data/NB501086_0103_DTremethick_JCSMR_HiC_shZ_TGFb_run3/HiC_6_r3/TGFb_rep1_r3_S3_R1_001.fastq.gz
+ln -s /home/skurscheid/Data/Tremethick/Breast/HiC/raw_data/NB501086_0103_DTremethick_JCSMR_HiC_shZ_TGFb_run3/HiC_6_r3/TGFb_rep1_r3_S3_R2_001.fastq.gz
+cd ..
+
+mkdir MCF10A_TGFb_2; cd MCF10A_TGFb_2
+ln -s /home/skurscheid/Data/Tremethick/Breast/HiC/raw_data/NB501086_0088_DTremethick_JCSMR_HiC_shZ_TGFb/HiC_7/TGFb_rep2_S4_R1_001.fastq.gz TGFb_rep2_r1_S4_R1_001.fastq.gz
+ln -s /home/skurscheid/Data/Tremethick/Breast/HiC/raw_data/NB501086_0088_DTremethick_JCSMR_HiC_shZ_TGFb/HiC_7/TGFb_rep2_S4_R2_001.fastq.gz TGFb_rep2_r1_S3_R2_001.fastq.gz
+ln -s /home/skurscheid/Data/Tremethick/Breast/HiC/raw_data/NB501086_0064_DTremethick_JCSMR_HiC_shZ_TGFb/HiC_7/TGFb-rep2_S4_R1_001.fastq.gz TGFb_rep2_r2_S4_R1_001.fastq.gz
+ln -s /home/skurscheid/Data/Tremethick/Breast/HiC/raw_data/NB501086_0064_DTremethick_JCSMR_HiC_shZ_TGFb/HiC_7/TGFb-rep2_S4_R2_001.fastq.gz TGFb_rep2_r2_S4_R2_001.fastq.gz
+ln -s /home/skurscheid/Data/Tremethick/Breast/HiC/raw_data/NB501086_0103_DTremethick_JCSMR_HiC_shZ_TGFb_run3/HiC_7_r3/TGFb_rep2_r3_S4_R1_001.fastq.gz
+ln -s /home/skurscheid/Data/Tremethick/Breast/HiC/raw_data/NB501086_0103_DTremethick_JCSMR_HiC_shZ_TGFb_run3/HiC_7_r3/TGFb_rep2_r3_S4_R2_001.fastq.gz
