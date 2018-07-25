f3af0a1ca6c7083649a661863eba6fa175f3e713
Sebastian Kurscheid <genomicsio@gmail.com>
2018-07-25 15:07:03 +1000

    adds meme and tomtom to workflow

--- snakemake/rules/shortFragments_macs2_peak_calling.py
+++ snakemake/rules/shortFragments_macs2_peak_calling.py
@@ -58,6 +58,57 @@ rule get_summit_sequences:
     script:
         home + "/Development/JCSMR-Tremethick-Lab/Breast/snakemake/scripts/prepare_summit_sequences.R"
 
+
+rule run_meme:
+    version:
+        "1.0"
+    params:
+        meme_bin = home + "/meme/bin/meme",
+        minw = 8,
+        maxw = 15,
+        nmotifs = 1000,
+        evt = 0.05, # e-value threshold
+        mod = "zoop"
+    threads:
+        10
+    input:
+        summitsSeqFile = home + "/Data/Collaborations/FSU/PromoterSeqCap/SmallFragments/macs2PeakCalling/{smallFragments}_summits.fasta",
+        promotersHMM = home + "/Data/Collaborations/FSU/PromoterSeqCap/SmallFragments/promoterSequences.hmm"
+    output:
+        meme_out = home + "/Data/Collaborations/FSU/PromoterSeqCap/SmallFragments/meme/{memeObjectiveFunction}/{smallFragments}",
+        memeOutput = home + "/Data/Collaborations/FSU/PromoterSeqCap/SmallFragments/meme/{memeObjectiveFunction}/{smallFragments}/meme.html"
+    shell:
+        """
+            {params.meme_bin} -oc {output.meme_out}\
+                              -dna \
+                              -bfile {input.promotersHMM} \
+                              -minw {params.minw} \
+                              -maxw {params.maxw} \
+                              -nmotifs {params.nmotifs} \
+                              -evt {params.evt} \
+                              -p {threads} \
+                              -objfun {wildcards.memeObjectiveFunction} \
+                              -mod {params.mod} \
+                              {input.summitSeqFile}
+        """
+
+rule run_tomtom:
+    version:
+        "1.0"
+    params:
+        tomtom_bin = home + "/meme/bin/tomtom"
+    threads:
+        1
+    input:
+        motifDB = home + "/Data/References/MEME/motif_databases/HUMAN/HOCOMOCOv11_full_HUMAN_mono_meme_format.meme",
+        memeOutput = home + "/Data/Collaborations/FSU/PromoterSeqCap/SmallFragments/meme/{memeObjectiveFunction}/{smallFragments}/meme.html"
+    output:
+        tomtom_out = home + "/Data/Collaborations/FSU/PromoterSeqCap/SmallFragments/tomtom/{memeObjectiveFunction}/{smallFragments}"
+    shell:
+        """
+            {params.tomtom} -oc {input.memeOutput} {motifDB}
+        """
+
 rule sort_bedGraph:
     input:
         macs2output = "/home/sebastian/Data/Collaborations/FSU/PromoterSeqCap/SmallFragments/macs2PeakCalling/{smallFragments}",
@@ -95,4 +146,14 @@ rule all:
                                   "TOTALcombined_CA1a_H2AZ_000-125",
                                   "TOTALcombined_CA1a_Inp_000-125",
                                   "TOTALcombined_shH2AZ_Inp_000-125"],
-                suffix = ["_treat_pileup.bw", "_summits.fasta"])
+                suffix = ["_treat_pileup.bw", "_summits.fasta"]),
+        expand(home + "/Data/Collaborations/FSU/PromoterSeqCap/SmallFragments/{meme_bin}/{memeObjectiveFunction}/{smallFragments}",
+                meme_bin = ["meme", "tomtom"],
+                memeObjectiveFunction = ["cd", "ce"],
+                smallFragments = ["TOTALcombined_A_H2AZ_000-125",
+                                  "TOTALcombined_A_Inp_000-125",
+                                  "TOTALcombined_A_TGFb_H2AZ_000-125",
+                                  "TOTALcombined_A_TGFb_Inp_000-125",
+                                  "TOTALcombined_CA1a_H2AZ_000-125",
+                                  "TOTALcombined_CA1a_Inp_000-125",
+                                  "TOTALcombined_shH2AZ_Inp_000-125"])
