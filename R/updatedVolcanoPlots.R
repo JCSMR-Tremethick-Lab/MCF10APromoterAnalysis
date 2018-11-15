56541cb7c3de0372b0fe012d3e0bf3b87324f649
Sebastian Kurscheid <genomicsio@gmail.com>
2018-11-16 10:03:39 +1100

    changes dataset used for visualisation

--- R/updatedVolcanoPlots.R
+++ R/updatedVolcanoPlots.R
@@ -9,11 +9,14 @@ sigEMTCellLines <- data.table::fread("https://raw.githubusercontent.com/skursche
 sigEMTTumors <- data.table::fread("https://raw.githubusercontent.com/skurscheid/GeneSets/master/Literature/Tan_et_al_2014/Thiery_generic_EMT_sig_tumor.txt")
 
 # load pre-processed expression data
-load("/home/sebastian/Data/Tremethick/Breast/RNA-Seq/combined/R_Analysis/sleuthResults_GRCh37_hg19_UCSC_V1.rda")
-rTshH2AZ <- results$sleuth_results_genes$conditionMCF10AshZD8
+load("~/Data/Tremethick/Breast/RNA-Seq_run2/NB501086_0082_RDomaschenz_JCSMR_mRNAseq/R_Analysis/sleuthResults_GRCh37_hg19_ensembl75_ERCC_V2.rda")
+
+rTshH2AZ <- as.data.table(results$MCF10A_vs_shZ$sleuth_results_genes$conditionMCF10A_shZ)
 setkey(rTshH2AZ, target_id)
-rTTGFb <-  results$sleuth_results_genes$conditionMCF10ATGFbD6
+rTshH2AZ <- rTshH2AZ[!is.na(pval)]
+rTTGFb <- as.data.table(results$MCF10A_vs_TGFb$sleuth_results_genes$conditionMCF10A_TGFb)
 setkey(rTTGFb, target_id)
+rTTGFb <- rTTGFb[!is.na(pval)]
 
 setkey(sigEMTCellLines, cellLine_sig)
 setkey(sigEMTTumors, tumor_sig)
@@ -50,10 +53,10 @@ volcPlotEMTshH2AZ <- ggplot(data = rTshH2AZ,
   geom_vline(xintercept = 0) +
   geom_hline(yintercept = 0)
 volcPlotEMTshH2AZ <- volcPlotEMTshH2AZ + geom_point(data = labelDatshH2AZ, aes(colour = epi_mes), size = 0.8)
-volcPlotEMTshH2AZ <- volcPlotEMTshH2AZ + geom_text_repel(data = rbind(labelDatshH2AZ[qval < 0.1][abs(b) > 2.75], labelDatTGFb[target_id %in% c("H2AFZ", "TGFB1")]), 
+volcPlotEMTshH2AZ <- volcPlotEMTshH2AZ + geom_text_repel(data = rbind(labelDatshH2AZ[qval < 0.1][abs(b) > 1.5], labelDatshH2AZ[target_id %in% c("H2AFZ", "TGFB1")]), 
                                   aes(label = target_id), 
                                   min.segment.length = 0,
-                                  size = 0.8)
+                                  size = 3)
 volcPlotEMTshH2AZ
 # highlight cfos, cmy, stat3
 targetGenes <- c("FOS", "MYC", "STAT3")
@@ -65,13 +68,18 @@ volcPlotEMTshH2AZ <- volcPlotEMTshH2AZ + geom_text_repel(data = rTshH2AZ[target_
 
 
 volcPlotEMTshH2AZ
+ggsave("~/Data/Collaborations/FSU/PromoterSeqCap/PublicationFigures/DifferentialExpression_shH2AZ_volcanoPlot.pdf", volcPlotEMTshH2AZ, scale = 2)
+write.csv(rTTGFb[order(qval, decreasing = F)], file = "~/Data/Collaborations/FSU/PromoterSeqCap/PublicationFigures/DifferentialExpression_shH2AZ_Table.csv")
+
 # volcano plot for WT vs TGFB ---------------------------------------------
+rm(labelDatTGFb)
 labelDatTGFb <- rbind(merge(rTTGFb, 
                             sigEMT, 
                             by.x = "target_id", 
                             by.y = "gene_symbol"))
 setkey(labelDatTGFb, target_id)
-labelDatTGFb["TGFB1"]
+
+
 
 volcPlotEMTTGFb <- ggplot(data = rTTGFb,
                           aes(x = b, y = -(log10(qval)))) +
@@ -83,5 +91,14 @@ volcPlotEMTTGFb <- volcPlotEMTTGFb + geom_text_repel(data = rbind(labelDatTGFb[q
                                                      aes(label = target_id), 
                                                      min.segment.length = 0,
                                                      size = 3)
+
+
+volcPlotEMTTGFb <- volcPlotEMTTGFb + geom_text_repel(data = rTTGFb[target_id %in% targetGenes], 
+                                                         aes(label = target_id), 
+                                                         min.segment.length = 0,
+                                                         size = 3)
 volcPlotEMTTGFb
+ggsave("~/Data/Collaborations/FSU/PromoterSeqCap/PublicationFigures/DifferentialExpression_TGFb_volcanoPlot.pdf", volcPlotEMTTGFb, scale = 2)
+write.csv(rTTGFb[order(qval, decreasing = F)], file = "~/Data/Collaborations/FSU/PromoterSeqCap/PublicationFigures/DifferentialExpression_TGFb_Table.csv")
+
 
