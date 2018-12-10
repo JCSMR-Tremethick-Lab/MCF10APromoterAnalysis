7242788060ccc9cc32ea4a0fe73ec57aa78129a0
Sebastian Kurscheid <genomicsio@gmail.com>
2018-12-10 14:57:25 +1100

    tidies up script

--- snakemake/rules/shortFragments_meme_processing.py
+++ snakemake/rules/shortFragments_meme_processing.py
@@ -66,22 +66,6 @@ rule run_tomtom:
             {params.tomtom_bin} -oc {output[0]} {input.memeOutput} {motifDB}
         """
 
-
-#rule motif_summary:
-#    version:
-#        "1.0"
-#    threads:
-#        1
-#    input:
-#        home + "/Data/Collaborations/FSU/PromoterSeqCap/SmallFragments/meme/{memeObjectiveFunction}/{smallFragments}"
-#    output:
-#        home + "/Data/Collaborations/FSU/PromoterSeqCap/SmallFragments/meme_summary/{memeObjectiveFunction}/{smallFragments}/summary.txt"
-#    shell:
-#        """
-#            grep "MOTIF" {input}/meme.txt > {output}
-#        """
-
-
 rule run_fimo: # to get sequences in which motifs can be found
     version:
         "1.0"
@@ -108,10 +92,3 @@ rule all:
         expand("/home/sebastian/Data/Collaborations/FSU/PromoterSeqCap/SmallFragments/fimo/{memeObjectiveFunction}/{smallFragments}",
                 memeObjectiveFunction = ["cd", "ce"],
                 smallFragments = ["TOTALcombined_shH2AZ_Inp_000-125"])
-                                  #"TOTALcombined_A_H2AZ_000-125",
-                                  #"TOTALcombined_A_Inp_000-125",
-                                  #"TOTALcombined_A_TGFb_H2AZ_000-125",
-                                  #"TOTALcombined_A_TGFb_Inp_000-125",
-                                  #"TOTALcombined_CA1a_H2AZ_000-125",
-                                  #"TOTALcombined_CA1a_Inp_000-125",
-                                  #"TOTALcombined_shH2AZ_Inp_000-125"])
